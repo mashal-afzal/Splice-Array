2de9473151d27b56cfb56415bc20db11681f2b3b
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-06-01 17:33:07 +0500

    Add files via upload

---
+++ splice.js
@@ -0,0 +1,4 @@
+var pakTeam = ["Afridi", "Sayeed Ajaml", "Muhammad Amir"];
+//console.log(pakTeam);
+pakTeam.splice(1, 2, "Babar Azam", "Muhammad Rizwan");
+console.log(pakTeam);
---
+++ splice.ts
@@ -0,0 +1,4 @@
+let pakTeam: string[]= ["Afridi", "Sayeed Ajaml", "Muhammad Amir"]
+//console.log(pakTeam);
+pakTeam.splice(1,2,"Babar Azam", "Muhammad Rizwan")
+console.log(pakTeam);
